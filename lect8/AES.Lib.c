09885f5b731d7a7fd78f0502fbc807aee6f61547
Giacomo-Fregona <giacomofregona99@gmail.com>
2022-11-21 16:14:06 +0100

    Material of lecture 8 uploaded (exercise mad during class)

--- lect8/AES.Lib.c
+++ lect8/AES.Lib.c
@@ -5,8 +5,8 @@
 
 void roundKeyGen(uint8_t roundKey[NR_ROUNDS+1][WORDS_IN_KEY][BYTES_IN_WORD], uint8_t Key[WORDS_IN_KEY][BYTES_IN_WORD]){
 	
-	for(int i = 0; i<4; i++){
-		for(int j = 0; j<4; j++){
+	for(int i = 0; i<WORDS_IN_KEY; i++){
+		for(int j = 0; j<BYTES_IN_WORD; j++){
 			roundKey[0][i][j] = Key[i][j];
 		}
 	}
@@ -15,15 +15,15 @@ void roundKeyGen(uint8_t roundKey[NR_ROUNDS+1][WORDS_IN_KEY][BYTES_IN_WORD], uin
 	for(int i = 1; i <= NR_ROUNDS; i++){
 		
 		// Prima word (prima riga di roundKey[i])
-		for(int k = 0; k < 4; k++) {
-			roundKey[i][0][(k-1)%4]	= SBox[roundKey[i][0][k]];
+		for(int k = 0; k < BYTES_IN_WORD; k++) {
+			roundKey[i][0][(k-1)%BYTES_IN_WORD]	= SBox[roundKey[i][0][k]];
 		}
 		
 		roundKey[i][0][0] ^= RCon[i];
 		
 		// Successive 3 righe di roundKey[i]
-		for(int j = 1; j < 4; j++) {
-			for(int k = 0; k < 4; k++){
+		for(int j = 1; j < WORDS_IN_KEY; j++) {
+			for(int k = 0; k < BYTES_IN_WORD; k++){
 				roundKey[i][j][k] = roundKey[i][j-1][k] ^ roundKey[i-1][j][k];
 			}
 		}
