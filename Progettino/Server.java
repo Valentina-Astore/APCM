76bd6af06e30a521e1d0ac76b98821cd6c7c5771
Giacomo-Fregona <giacomofregona99@gmail.com>
2022-12-02 20:54:14 +0100

    Final version of progettino

--- Progettino/Server.java
+++ Progettino/Server.java
@@ -6,78 +6,96 @@ import java.net.*;
 
 public class Server {
     static int i; static int j;
+
     
     public static void main(String[] args){
-
-		byte Key[]={'p','i','p','p','o'};//dobbiamo farla di 128 bit e formato AES
-
-		AES_CFB cipher = new AES_CFB();
-		
-		try {
-			ServerSocket serverSocket=new ServerSocket(2022);
-			Socket s=serverSocket.accept();
-
-//	        Ricezione titolo del file da inviare.
-
+    
+    
+        try{
+            //Lancio dei processi ./AES_CFB_E Server e ./AES_CFB_D Server. Saranno utilizzati nella cifratura e decifratura delle comunicazioni. Per aggirare il problema che process Builder non permetta di lanciare processi in background, lanciamo i processi su due terminali accessori che verranno aperti e si chiuderanno al termine del processo	
+            ProcessBuilder pD = new ProcessBuilder();
+            pD.command("bash", "-c","gnome-terminal -- ./AES_CFB_D Server");
+            Process processD = pD.start();
+    
+            ProcessBuilder pE = new ProcessBuilder();
+            pE.command("bash", "-c","gnome-terminal -- ./AES_CFB_E Server");
+            Process processE = pE.start();
+            
+            
+            //Apertura e setup dei due processi di cifratura/decifratura. Ci assicuriamo con try-catch che vengano sicuramente entrambi chiusi             
+            AES_CFB_E cipherE = new AES_CFB_E();
+		    AES_CFB_D cipherD = new AES_CFB_D();
+		    cipherD.Setup("Server");
+		    cipherE.Setup("Server");	
+		    
+		    	    
+	        //Creazione del canale di comunicazione con il Client. 
+	        ServerSocket serverSocket=new ServerSocket(2022);
+		    Socket s=serverSocket.accept();
 		    DataInputStream reader=new DataInputStream(s.getInputStream());
-		    
-//	        Il primo elemento letto è la lunghezza della stringa del titolo, poi vengono letti uno ad uno i caratteri del titolo.
-		    int inputLength = reader.readInt();
-		    byte[] titleBytes = new byte[inputLength];
-
-		    for(i = 0; i < inputLength; i++){
-		        titleBytes[i] = reader.readByte();
-		    }
-
-//			Decryption titolo.
-			cipher.Setup("Server");
-			
-			String title = cipher.decrypt(titleBytes);
-			System.out.println("Titolo ricevuto: "+title+"\n");
-			
-			cipher.close();
-
-
-//			Apertura del file da inviare.
-		    String testo = "";
-		    try {
-		        File contentFile = new File("./ServerDatabase/"+title+".txt");
-		        FileInputStream contentStream = new FileInputStream(contentFile);
-		        
-		        char c;
-		        while((c = (char) contentStream.read()) != (char)-1){
-					testo +=  c;
-		        }
-		        
-		        System.out.println("Testo da inviare:\n" + testo);
-		        //System.out.println();
-		        
-		    } catch(Exception e) {
-			    System.out.println("Error in opening the file from ServerDatabase");
-			    e.printStackTrace();
-		    }
-
-//			Cifratura e invio contenuto file.
-
-			cipher.Setup("Server");
-		    
 		    DataOutputStream writer = new DataOutputStream(s.getOutputStream());
 		    
-		    byte contentBytes[] = cipher.encrypt(testo);
 		    
-		    writer.writeInt(contentBytes.length);
-		    for(i = 0; i < contentBytes.length; i++){
-		        writer.writeByte(contentBytes[i]);
+		    try {
+//              #########   QUERY DEL CLIENT   #########
+		        int inputLength = reader.readInt();//Acquisiamola lunghezza del nome utente richiesto
+		        
+		        //Acquisiamo il cifrato del nome utente
+		        byte[] userBytes = new byte[inputLength];
+		        for(i = 0; i < inputLength; i++){
+		            userBytes[i] = reader.readByte();
+		        }
+
+			    
+			    //Decifriamo il nome utente
+			    String user = cipherD.decrypt(userBytes);
+			    
+			    //Stampa a video del nome utente
+			    System.out.println("Nome utente ricevuto: "+user);
+			    
+
+                //Apertura del file da inviare
+		        String content = "";
+		        File contentFile = new File("./ServerDatabase/"+user+".txt");
+	            FileInputStream contentStream = new FileInputStream(contentFile);
+		        try {
+		            char c;
+		            while((c = (char) contentStream.read()) != (char)-1){
+					    content +=  c;
+		            }
+		        } catch(IOException e) {
+		            throw new IOException("Error in opening the file from ServerDatabase.");
+		        } finally {contentStream.close();}
+		        
+		        
+		        //Stampa a video del contenuto da inviare
+		        System.out.println("\nContenuto da inviare:\n"+content);
+		        
+		        
+		        //Cifratura del contenuto
+		        byte contentBytes[] = cipherE.encrypt(content);
+		        
+		        
+		        //Invio del cifrato del contenuto
+		        writer.writeInt(contentBytes.length);
+		        for(i = 0; i < contentBytes.length; i++){
+		            writer.writeByte(contentBytes[i]);
+		        }
+
+
+		    } catch(IOException ex){
+			    ex.printStackTrace();
+		    } finally {
+			    cipherE.close();
+			    cipherD.close();
+			    reader.close();
+			    writer.close();
 		    }
-
-			reader.close();
-			writer.close();
-
-		} catch(IOException ex){
-			ex.printStackTrace();
-		} finally {
-			cipher.close();
-		}
+		    
+        }catch(IOException ex){
+		    ex.printStackTrace();
+	    }
+    
 	
 	}
 }
